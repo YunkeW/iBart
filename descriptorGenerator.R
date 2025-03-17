58da07fd8ff4173b69c7e036f61fa03f2f71b30a
YunkeW <156373849+YunkeW@users.noreply.github.com>
2025-03-16 21:47:27 -0400

    Update descriptorGenerator.R

--- descriptorGenerator.R
+++ descriptorGenerator.R
@@ -1,14 +1,19 @@
-descriptorGenerator <- function(data = NULL, opt = "binary", sin_cos = FALSE, apply_pos_opt_on_neg_x = TRUE, verbose = TRUE) {
+descriptorGenerator <- function(data = NULL,
+                                opt = "binary",
+                                sin_cos = FALSE,
+                                apply_pos_opt_on_neg_x = TRUE, 
+                                allowed_ops = c("multiply", "divide", "sqrt", "inverse", "square", "log", "sin", "cos"), # Allowed Operators list
+                                verbose = TRUE) {
   if (opt == "binary") {
     if (verbose) cat("Constructing descriptors using binary operators... \n")
-    data <- binary(data, sin_cos)
+    data <- binary(data, sin_cos, allowed_ops)
   } else if (opt == "unary") {
     if (verbose) cat("Constructing descriptors using unary operators... \n")
-    data <- unary(data, sin_cos, apply_pos_opt_on_neg_x)
+    data <- unary(data, sin_cos, apply_pos_opt_on_neg_x, allowed_ops)
   } else {
     if (verbose) cat("Constructing descriptors using all operators... \n")
-    data_unary <- unary(data, sin_cos, apply_pos_opt_on_neg_x)
-    data_binary <- binary(data, sin_cos)
+    data_unary <- unary(data, sin_cos, apply_pos_opt_on_neg_x, allowed_ops)
+    data_binary <- binary(data, sin_cos, allowed_ops)
     data$X <- cbind(data_unary$X, data_binary$X)
     data$name <- c(data_unary$name, data_binary$name)
     data$unit <- cbind(data_unary$unit, data_binary$unit)
