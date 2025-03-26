71df0e99d84d0aa353e4e5c65b6e931f56b8f8ee
YunkeW <156373849+YunkeW@users.noreply.github.com>
2025-03-25 21:32:22 -0400

    Update descriptorGenerator.R

--- descriptorGenerator.R
+++ descriptorGenerator.R
@@ -1,23 +1,28 @@
 descriptorGenerator <- function(data = NULL,
+                                y,
                                 opt = "binary",
                                 sin_cos = FALSE,
                                 apply_pos_opt_on_neg_x = TRUE, 
-                                allowed_ops = c("multiply", "divide", "sqrt", "inverse", "square", "log", "sin", "cos"), # Allowed Operators list
-                                verbose = TRUE) {
+                                allowed_ops = c("multiply", "divide", "sqrt", "inverse", "square", "log", "sin", "cos"),
+                                verbose = TRUE,
+                                cor_threshold = 0.2,
+                                grid_seq = seq(-1, 1, by = 0.5)) {
   if (opt == "binary") {
     if (verbose) cat("Constructing descriptors using binary operators... \n")
-    data <- binary(data, y, allowed_ops, sin_cos = FALSE)
+    data <- binary(data, y, allowed_ops, sin_cos)
   } else if (opt == "unary") {
     if (verbose) cat("Constructing descriptors using unary operators... \n")
-    data <- unary(data, sin_cos, apply_pos_opt_on_neg_x, allowed_ops)
+    data <- unary(data, y, sin_cos, apply_pos_opt_on_neg_x, cor_threshold, grid_seq)
   } else {
     if (verbose) cat("Constructing descriptors using all operators... \n")
-    data_unary <- unary(data, y, sin_cos, apply_pos_opt_on_neg_x)
-    data_binary <- binary(data, y, allowed_ops, sin_cos = FALSE)
+    data_unary <- unary(data, y, sin_cos, apply_pos_opt_on_neg_x, cor_threshold, grid_seq)
+    data_binary <- binary(data, y, allowed_ops, sin_cos)
+  
     data$X <- cbind(data_unary$X, data_binary$X)
     data$name <- c(data_unary$name, data_binary$name)
     data$unit <- cbind(data_unary$unit, data_binary$unit)
     data <- dataprocessing(data)
   }
+
   return(data)
 }
