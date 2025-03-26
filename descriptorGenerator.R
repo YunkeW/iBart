77dae2b04fe0b0b564b0ea30e0b3890d964b29e4
YunkeW <156373849+YunkeW@users.noreply.github.com>
2025-03-25 21:35:14 -0400

    Update descriptorGenerator.R

--- descriptorGenerator.R
+++ descriptorGenerator.R
@@ -3,7 +3,8 @@ descriptorGenerator <- function(data = NULL,
                                 opt = "binary",
                                 sin_cos = FALSE,
                                 apply_pos_opt_on_neg_x = TRUE, 
-                                allowed_ops = c("multiply", "divide", "sqrt", "inverse", "square", "log", "sin", "cos"),
+                                allowed_ops = c("add", "minus", "multiply", "divide", "sqrt", "inverse", 
+                                                "square", "log", "sin", "cos", "abs", "exp"),
                                 verbose = TRUE,
                                 cor_threshold = 0.2,
                                 grid_seq = seq(-1, 1, by = 0.5)) {
@@ -17,12 +18,12 @@ descriptorGenerator <- function(data = NULL,
     if (verbose) cat("Constructing descriptors using all operators... \n")
     data_unary <- unary(data, y, sin_cos, apply_pos_opt_on_neg_x, cor_threshold, grid_seq)
     data_binary <- binary(data, y, allowed_ops, sin_cos)
-  
+    
     data$X <- cbind(data_unary$X, data_binary$X)
     data$name <- c(data_unary$name, data_binary$name)
-    data$unit <- cbind(data_unary$unit, data_binary$unit)
+    data$unit <- if (!is.null(data$unit)) cbind(data_unary$unit, data_binary$unit) else NULL
+    
     data <- dataprocessing(data)
   }
-
   return(data)
 }
